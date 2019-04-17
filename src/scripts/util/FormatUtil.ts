f0e12feb1649cd9ee47e5bbc427945149a4ac899
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-04-16 17:18:15 -0700

    Unified currency parameters.

--- src/scripts/util/FormatUtil.ts
+++ src/scripts/util/FormatUtil.ts
@@ -4,17 +4,49 @@ export function decimal(value: number, digits: number = 0) {
 }
 
 export function currency(
-    value: number,
-    locales: string | string[] = 'english',
-    currency: string = 'USD',
+    value: number | string,
+    locales?: string | string[],
+    options?: Intl.NumberFormatOptions
+): string;
+export function currency(
+    value: number | string,
+    locales?: string | string[],
+    currency?: string,
     digits?: number
-) {
-    value = value || 0;
-    return value.toLocaleString(locales, {
-        style: 'currency',
-        currency: currency,
-        minimumFractionDigits: digits
-    });
+): string;
+export function currency(
+    value: number | string,
+    locales: string | string[] = 'english',
+    currency: string | Intl.NumberFormatOptions = 'USD',
+    digits?: number
+): string {
+    switch (typeof value) {
+        case 'string':
+            value = parseFloat(value) || 0;
+            break;
+        case 'number':
+            value = value || 0;
+            break;
+        default:
+            value = 0;
+            break;
+    }
+    switch (typeof currency) {
+        case 'string':
+            return value.toLocaleString(locales, {
+                style: 'currency',
+                currency: currency,
+                minimumFractionDigits: digits
+            });
+        case 'object':
+            let options = Object.assign({}, {
+                style: 'currency',
+                minimumFractionDigits: digits
+            }, currency);
+            return value.toLocaleString(locales, options);
+        default:
+            return '' + value;
+    }
 }
 
 export function date(value: string | Date, locales?: string | string[], options?: Intl.DateTimeFormatOptions) {
